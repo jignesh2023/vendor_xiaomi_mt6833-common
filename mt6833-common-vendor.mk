205ce8af51a41c0dce408dc097d426ce9d83616c
Taqi Raza <mttalpur@gmail.com>
2023-12-07 20:00:37 +0500

    everpal: sync proprietary-files

--- mt6833-common-vendor.mk
+++ mt6833-common-vendor.mk
@@ -172,7 +172,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libfmjni \
-    libvsim-adaptor-client \
     arm.graphics-V1-ndk_platform \
     android.hardware.bluetooth@1.1-impl-mediatek \
     memtrack.default \
